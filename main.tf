a283fe92bc4bd9423d9eef930d98cffaf6161993
Harneet Singh <34583005+harneet-9039@users.noreply.github.com>
2021-05-27 12:22:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,26 @@
-locals {
-  name_prefix = "${var.general["name"]}-${var.general["region"]}"
+provider "google" {
+
+  version = "~> 3.0"
 }
 
-# Instance CloudSQL
-# https://www.terraform.io/docs/providers/google/r/sql_database_instance.html
-resource "google_sql_database_instance" "new_instance_sql" {
-  name             = "${local.name_prefix}"
-  region           = "${var.general["region"]}"
-  database_version = "${lookup(var.general, "db_version", "MYSQL_5_7")}"
+module "instance_template" {
+  source          = "../../../modules/instance_template"
+  region          = var.region
+  project_id      = var.project_id
+  subnetwork      = var.subnetwork
+  service_account = var.service_account
+}
 
-  settings {
-    tier                        = "${lookup(var.instance, "tier", "db-f1-micro")}"
-    disk_type                   = "${lookup(var.instance, "disk_type", "PD_SSD")}"
-    disk_size                   = "${lookup(var.instance, "disk_size", 10)}"
-    disk_autoresize             = "${lookup(var.instance, "disk_auto", true)}"
-    activation_policy           = "${lookup(var.instance, "activation_policy", "ALWAYS")}"
-    availability_type           = "${lookup(var.instance, "availability_type", "ZONAL")}"
-    authorized_gae_applications = "${var.authorized_gae_applications}"
-    user_labels                 = "${var.labels}"
-
-    ip_configuration {
-      require_ssl  = "${lookup(var.instance, "require_ssl", false)}"
-      ipv4_enabled = "${lookup(var.instance, "ipv4_enabled", true)}"
-    }
-
-    location_preference {
-      zone = "${var.general["region"]}-${var.instance["zone"]}"
-    }
-
-    backup_configuration {
-      binary_log_enabled = false
-      enabled            = "${lookup(var.general, "backup_enabled", true)}"
-      start_time         = "${lookup(var.general, "backup_time", "02:30")}" # every 2:30AM
-    }
-
-    maintenance_window {
-      day          = "${lookup(var.instance, "maintenance_day", 1)}"          # Monday
-      hour         = "${lookup(var.instance, "maintenance_hour", 2)}"         # 2AM
-      update_track = "${lookup(var.instance, "maintenance_track", "stable")}"
-    }
-  }
+module "compute_instance" {
+  source            = "../../../modules/compute_instance"
+  region            = var.region
+  zone              = var.zone
+  subnetwork        = var.subnetwork
+  num_instances     = var.num_instances
+  hostname          = "instance-simple"
+  instance_template = module.instance_template.self_link
+  access_config = [{
+    nat_ip       = var.nat_ip
+    network_tier = var.network_tier
+  }, ]
 }
